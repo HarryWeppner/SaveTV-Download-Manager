da8bcbf29e7205621e436268b7ccb69f482cfcd1
IngoS11 <ingo@simino.net>
2010-10-11 08:07:43 -0700

    Added Recording TestCase

---
+++ src/com/ingo/savetv/junit/RecordingTest.java
@@ -0,0 +1,19 @@
+package com.ingo.savetv.junit;
+
+import junit.framework.TestCase;
+
+public class RecordingTest extends TestCase {
+	
+	public RecordingTest(){
+		super();
+	}
+	
+	protected void setUp() throws Exception {
+		
+	}
+	
+	protected void tearDown() throws Exception {
+		
+	}
+
+}
